cf21f4595be34fbf6a4332effa6e4d773c575445
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-06-16 23:30:48 +0300

    add MenuBar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import {SocialMedia} from "./SocialMedia";
+import {MenuBar} from "./Menu";
 
 export function Header() {
 
     return (
         <header style={{flexGrow: 1}}>
-            <AppBar position="static">
+            <AppBar position="static" style={{background: "#222"}}>
                 <Toolbar>
-                    <Typography variant="h6">
-                        Little blog
-                    </Typography>
+                    <MenuBar/>
+                    <NavLink to="/">
+                        <Typography variant="h6">
+                            Little blog
+                        </Typography>
+                    </NavLink>
                     <div style={{flexGrow: 1}}/>
                     <SocialMedia/>
                 </Toolbar>
