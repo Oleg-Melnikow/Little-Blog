3f626d1fee48e3d6600a042e105cdaf86839b483
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-06-17 14:51:18 +0300

    add pagination

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,19 +2,45 @@ import React from 'react';
 import {postType} from "../../state/blogReducer";
 import {Grid} from "@material-ui/core";
 import {PostCard} from "../PostCard/PostCard";
+import s from "./MainPage.module.css"
+import {PaginationRounded} from "../Pagination/Pagination";
 
 type MainPagePropsType = {
     posts: Array<postType>,
+    currentPage: number,
+    pageSize: number,
+    handlerNextPage: (page: number) => void
 }
 
-export function MainPage(props: MainPagePropsType) {
-    return (
-        <div>
-            <Grid container direction="row" justify="center" style={{minHeight: "600px"}}>
-                {props.posts.map(post => {
-                    return <PostCard key={post.id} post={post}/>
-                })}
-            </Grid>
-        </div>
-    )
+export class MainPage extends React.Component<MainPagePropsType> {
+
+    nextPage = (page: number) => {
+        this.props.handlerNextPage(page);
+    }
+
+    componentWillUnmount() {
+        this.props.handlerNextPage(1);
+    }
+
+    render() {
+
+        let pageCount = Math.ceil(this.props.posts.length / this.props.pageSize);
+        let lowerBound = this.props.currentPage * this.props.pageSize - this.props.pageSize;
+        let upperBound = this.props.currentPage * this.props.pageSize;
+
+        return (
+            <div>
+                <Grid container direction="row" justify="center"  style={{minHeight: "600px"}}>
+                    {this.props.posts.filter(p => p.id > lowerBound && p.id <= upperBound)
+                        .map(post => {
+                            return <PostCard key={post.id} post={post}/>
+                        })}
+                </Grid>
+                {pageCount >= 2 && <div className={s.pagination}>
+                    <PaginationRounded currentPage={this.props.currentPage} totalPages={pageCount} isDisabled={false}
+                                       nextPage={this.nextPage}/>
+                </div>}
+            </div>
+        )
+    }
 }
